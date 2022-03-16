ef578a0aaf567292cbb9f1466794a3d3845f40ba
Codeno-ob <97269929+Codeno-ob@users.noreply.github.com>
2022-03-16 06:53:25 +0530

    Delete college py projects directory

--- college py projects/exp2.py
+++
@@ -1,9 +0,0 @@
-cor = input().split()
-x1=int(cor[0])
-x2=int(cor[1])
-y1=int(cor[2])
-y2=int(cor[3])
-a=(x2-x1)**2
-b=(y2-y1)**2
-dist=(a+b)**.5
-print("distance between two points is: ",round(dist,3))
--- college py projects/exp4.py
+++
@@ -1,6 +0,0 @@
-n = int(input('Enter number:'))
-
-print("COUNTDOWN BEGINS")
-while(n>=0):
-    print(n,end=' ')
-    n-=1
